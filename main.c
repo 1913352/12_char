9fe543bb670a42ea64b1ca9e9100f848d231e8d1
1913352 <55537201+1913352@users.noreply.github.com>
2019-11-21 15:20:22 +0900

    5

--- main.c
+++ main.c
@@ -5,11 +5,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+	int i;
+	char input[100];
+	FILE *fp;
 	
-	strcpy(dst, src);
-	
-	printf("copied string : %s", dst);
+	fp = fopen("sample.txt", "w");
+	for (i=0;i<3;i++)
+	{
+		printf("input a word : ");
+		scanf("%s", input);
+		fprintf(fp, "%s\n", input);
+	}
+	fclose(fp);
 	return 0;
 }
